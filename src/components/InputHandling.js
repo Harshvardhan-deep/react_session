5fefefbec1daaea3f4f00fa36b871321b417548f
HARSHVARDHAN <182997307+MrHarshvardhann@users.noreply.github.com>
2026-08-17 00:11:24 +0530

    Use-Effect

--- src/components/InputHandling.js
+++ src/components/InputHandling.js
@@ -1,17 +1,52 @@
-import React from "react";
+import React from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { ChangeEvent } from 'react';
+
 
 const InputHandling = () => {
 
-    /*const [name, setName] = useState('');
-    const [city, setCity] = useState('');*/
+    const [name, setName] = useState('');
+    const [city, setCity] = useState('');
 
     const submitfn = () => {
-        console.log('name', 'city');
+        console.log('name & city', name, city);
     }
 
     const changeHandler = (event) => {
         console.log(event.target.value);
+        setName(event.target.value);
     }
+
+    const changeHandler1 = (event) => {
+        console.log(event.target.value);
+        setCity(event.target.value);
+    }
+
+    //componentDidMount + componentDidUpdate
+    useEffect(() => {
+        console.log('USEEFFECT TIGGER (EveryTime).....');
+    })
+
+    //componentDidMount
+    useEffect(() => {
+        console.log('USEEFFECT TIGGER (didmount).....');
+    }, [])
+
+    //componentWillUnmount
+    useEffect(() => {
+        console.log('USEEFFECT TIGGER (willunmount).....');
+    }, [])
+
+    //componentDidUpdate on Condition
+    useEffect(() => {
+        console.log('USEEFFECT TIGGER (condition).....');
+    }, [name])
+
+    //componentDidUpdate on Condition
+    useEffect(() => {
+        console.log(name, city);
+    }, [name, city])
 
     return (
         <>
@@ -20,11 +55,22 @@ const InputHandling = () => {
             <div style={{ background: 'lightpink' }}>
                 <br></br>
                 Name: <input type="text" name="" onChange={changeHandler} />
+                {/*Name: <input type="text" name="" onBlur={changeHandler} />*/}
                 <br></br>
-                City: <input type="text" name="" onChange={changeHandler} />
+                City: <input type="text" name="" onChange={changeHandler1} />
+
+                <br></br>
                 <br></br>
 
                 <button onClick={submitfn}>Submit</button>
+
+                <br></br>
+                <br></br>
+
+                <div style={{ backgroundColor: 'yellow', padding: '10px' }} >Submitted Data... {name} & {city}</div>
+
+                <br></br>
+
             </div>
         </>
     )
